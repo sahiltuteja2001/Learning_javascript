954786058310d563edd36ba9a627f9f60027ca31
sahiltuteja2001 <sahiltuteja73@gmail.com>
2024-02-20 16:54:00 +0530

    learning variables

---
+++ 1_variables.js
@@ -0,0 +1,29 @@
+/*
+variables:
+let- 
+, const , var
+*/
+
+// let message; declaration
+// message= "hey , how are you?";
+
+// declaration + assignment
+let message= "hey , how are you?";
+console.log(message);
+
+let a=11,b=22,c=33;
+console.log(a);
+console.log(b);
+console.log(c);
+
+//main property
+message="hey ,i am fine";
+console.log(message);
+
+// creating constant in javascript
+
+const user=" SahilTuteja";
+console.log(user);
+// user="Sahil";
+
+
